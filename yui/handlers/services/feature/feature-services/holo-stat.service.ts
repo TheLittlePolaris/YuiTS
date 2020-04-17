ac20b6e00026ee0bd027e2db0ebb4d1d6d6715b6
NgSonHai <son-hai.nguyen@ant-tech.eu>
2020-04-17 21:41:53 +0700

    change to linux environment

---
+++ yui/handlers/services/feature/feature-services/holo-stat.service.ts
@@ -0,0 +1,8 @@
+import { debugLogger } from '@/handlers/log.handler'
+
+export class HoloStatService {
+  constructor() {
+    debugLogger('HoloStatService')
+  }
+  public hololiveOfficialChannelId = 'UCJFZiqLMntJufDCHc6bQixg'
+}
