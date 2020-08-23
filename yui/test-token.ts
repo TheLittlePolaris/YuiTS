09dfddacf1963d07221198fac74ab9fb2de187be
haihayta1999@gmail.com <son-hai.nguyen@ant-tech.eu>
2020-08-23 17:35:56 +0700

    Update

--- yui/test-token.ts
+++
@@ -1,5 +0,0 @@
-// import { InjectionToken } from './decorators/di-components'
-// import { CONST_TOKEN } from './constants/constants'
-
-// export const BOT_TOKEN = new InjectionToken(CONST_TOKEN.BOT_TOKEN)
-// export const PREFIX = new InjectionToken(CONST_TOKEN.BOT_PREFIX)
